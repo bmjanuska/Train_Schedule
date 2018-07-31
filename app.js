ac49e16e22fc151aab844ecb7b3aeb53bb815766
Bernadette Januska <bmjanuska@gmail.com>
2018-07-31 17:09:28 -0400

    initial file upload. foundation index and started logic

---
+++ app.js
@@ -0,0 +1,31 @@
+// Initialize Firebase
+
+// Button to add trains
+
+// Grabbing the user input
+
+// Creating local "temporary" object for holding train info
+
+// Upload train info to database 
+
+//log everything to console. testing
+
+//alert that an employee has been successfully added
+
+// clear inputs from the text boxes
+
+// firebase event for adding train to the database
+// also adding rows in our table from what the user added in the form 
+
+// storing everything into a variable 
+
+// train info console.log
+
+// moment calculations for the next arrival
+
+// moment calculations for minuets away
+
+// creating a new row for the DOM
+
+// appending the new row to the body 
+
